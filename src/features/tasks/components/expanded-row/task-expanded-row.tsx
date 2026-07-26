0e6eb89446d131a71b3c8171d6641a909a53fd01
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-26 17:53:39 -0500

    Update task-expanded-row.tsx

--- src/features/tasks/components/expanded-row/task-expanded-row.tsx
+++ src/features/tasks/components/expanded-row/task-expanded-row.tsx
@@ -105,7 +105,7 @@ export function TaskExpandedRow({
           <EntityExpandedToggle
             value={activeView}
             onChange={handleViewChange}
-            fullWidth
+            fullWidth={isMobile}
             options={[
               {
                 value: "workflow",
