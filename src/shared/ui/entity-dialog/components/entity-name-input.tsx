00e90f1feb72ea119e5a279b82777b81ec66c7bd
subvrik-sys32 <joel.sebastian.aguilar.huaman@gmail.com>
2026-07-30 13:10:44 -0500

    newbitacora2

--- src/shared/ui/entity-dialog/components/entity-name-input.tsx
+++ src/shared/ui/entity-dialog/components/entity-name-input.tsx
@@ -44,7 +44,7 @@ export const EntityNameInput=
             })
 
           }}
-          className="text-neutral-200 placeholder:text-neutral-600"
+          className="text-neutral-200 uppercase placeholder:text-neutral-600"
         />
 
       )
